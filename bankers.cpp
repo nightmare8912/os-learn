61097bf2d51630805dfb87d7f5a798913d85c1a3
Pratyush <pratyush12345786@gmail.com>
2023-04-04 19:13:25 +0530

    changed bankers

--- bankers.cpp
+++ bankers.cpp
@@ -1,12 +1,141 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
+class returnType
+{
+    public:
+    int resource_number, pid;
+    returnType()
+    {
+        resource_number = -1;
+        pid =-1;
+    }
+};
+void getInput(int available[], int max[][10], int allocation[][10], int need[][10], int num_processes, int num_resources)
+{   
+    cout << "Enter the values for available resources" << endl;
+    for (int i = 0; i < num_resources; i++)
+    {
+        printf("Enter available instances of R%d: ", i);
+        // cin >> available[i];
+        available[i] = rand() % 10;
+        cout << available[i] << endl;
+    }
+    for (int i = 0; i < num_processes; i++)
+    {
+        for (int j = 0; j < num_resources; j++)
+        {
+            printf ("Enter the maximum resources that P%d may request of resource type R%d: ", i, j);
+            // cin >> max[i][j];
+            max[i][j] = rand () % 10 + 1;
+            cout << max[i][j] << endl;
+            printf ("Enter the resources that P%d has been allocated of resource type R%d: ", i, j);
+            // cin >> allocation[i][j];
+            allocation[i][j] = rand() % max[i][j];
+            cout << allocation[i][j] << endl;
+            need[i][j] = max[i][j] - allocation[i][j];
+        }
+    }
+}
+void print(int available[], int max[][10], int allocation[][10], int need[][10], int num_processes, int num_resources)
+{
+    cout << "Printing available matrix: " << endl;
+    for (int i = 0; i < num_resources; i++)
+        cout << available[i] << "\t";
+    cout << endl;    
+    cout << "Printing max matrix: " << endl;
+    for (int i = 0; i < num_processes; i++)
+    {
+        for (int j = 0; j < num_resources; j++)
+            cout << max[i][j] << "\t";
+        cout << endl;    
+    }    
+    cout << "Printing allocation matrix: " << endl;
+    for (int i = 0; i < num_processes; i++)
+    {
+        for (int j = 0; j < num_resources; j++)
+            cout << allocation[i][j] << "\t";
+        cout << endl;    
+    }    
+    cout << "Printing need matrix: " << endl;
+    for (int i = 0; i < num_processes; i++)
+    {
+        for (int j = 0; j < num_resources; j++)
+            cout << need[i][j] << "\t";
+        cout << endl;    
+    }    
+}
+bool isComplete(bool finish[], int num_processes)
+{
+    for (int i = 0; i < num_processes; i++)
+    {
+        if (!finish[i])
+            return false;
+    }
+    return true;
+}
+returnType findProcToExec(returnType r, int need[][10], bool finish[], int work[], int num_processes, int num_resources)
+{
+    r.pid = -2;
+    for (int i = 0; i < num_processes; i++)
+    {
+        for (int j = 0; j < num_resources; j++)
+        {
+            if (finish[i] == false && need[i][j] <= work[j])
+            {
+                r.pid = i;
+                r.resource_number = j;
+                return r;
+            }
+        }
+    }
+    return r;
+}
+void execute(int available[], int max[][10], int allocation[][10], int need[][10], int num_processes, int num_resources)
+{
+    // Initializing
+    bool finish[num_processes];
+    int work[num_resources];
+    returnType r;
+    for (int i = 0; i < num_resources; i++)
+    {
+        work[i] = available[i];
+        finish[i] = false;
+    }
+    cout << endl;
+    while(r.pid != -2)
+    {
+        r = findProcToExec(r, need, finish, work, num_processes, num_resources);
+        if (r.pid == -2)
+            break;
+        work[r.pid] = work[r.pid] + allocation[r.pid][r.resource_number];
+
+        cout << "\nAvaialable resources after executing P" << r.pid << ": \n";
+        for (int i = 0; i < num_resources; i++)
+            cout << work[i] << "\t";
+        cout << endl;    
+
+        finish[r.pid] = true;
+        cout << "P" << r.pid << "\t";
+    }
+    cout << endl;
+    if(isComplete(finish, num_processes))
+        cout << "The system is in a safe state" << endl;
+    else
+        cout << "The system is not in a safe state" << endl;    
+}
 int main()
 {
     int num_resources, num_processes;
     cout << "Enter resources available and the number of processes: ";
     cin >> num_resources >> num_processes;
-    int available[num_resources], max[num_processes][num_resources]; 
-    int allocation[num_processes][num_resources], need[num_processes][num_resources];   
+    srand((long int)clock());
+    if (num_processes > 10)
+        num_processes = 10;
+    int available[num_resources], max[num_processes][10]; 
+    int allocation[num_processes][10], need[num_processes][10];  
+    getInput(available, max, allocation, need, num_processes, num_resources); 
     // execute(available, max, allocation, need, num_processes, num_resources);
-       
+    print(available, max, allocation, need, num_processes, num_resources); 
+    execute(available, max, allocation, need, num_processes, num_resources);
 }
