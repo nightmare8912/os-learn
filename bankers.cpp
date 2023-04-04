1522b50952e29762f3d97f576407a7ec2f249fae
Pratyush <pratyush12345786@gmail.com>
2023-04-04 15:23:54 +0530

    changed bankers

--- bankers.cpp
+++ bankers.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    int num_resources, num_processes;
+    cout << "Enter resources available and the number of processes: ";
+    cin >> num_resources >> num_processes;
+    int available[num_resources], max[num_processes][num_resources]; 
+    int allocation[num_processes][num_resources], need[num_processes][num_resources];   
+    // execute(available, max, allocation, need, num_processes, num_resources);
+       
+}
